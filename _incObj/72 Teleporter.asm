cb52d422c3553c07681299e9b13b6887e9369644
crashmahoney <crashmahoney@gmail.com>
2015-11-09 23:10:44 +1100

    got CPZ style tubes working with the SBZ teleport object, and did a bunch of layout stuff to GHZ

--- _incObj/72 Teleporter.asm
+++ _incObj/72 Teleporter.asm
@@ -1,6 +1,11 @@
 ; ---------------------------------------------------------------------------
 ; Object 72 - teleporter (SBZ)
 ; ---------------------------------------------------------------------------
+tele_coords_left = $3A		; number of coords left to go to *4 (2 bytes)
+tele_list_pos	= $3C		; Rom address of x y coordinates (4 bytes)
+tele_x_target	= $36		; target for sonic X (2 bytes)
+tele_y_target	= $38		; target for sonic Y (2 bytes)
+; ---------------------------------------------------------------------------
 
 Teleport:				; XREF: Obj_Index
 		moveq	#0,d0
@@ -10,16 +15,16 @@ Teleport:				; XREF: Obj_Index
 		obRanges	@delete
 		rts	
 
-	@delete:
+@delete:
         if S3KObjectManager=1
 		obMarkGone
         endif
 		jmp	DeleteObject
 ; ===========================================================================
 Tele_Index:	dc.w Tele_Main-Tele_Index
-		dc.w loc_166C8-Tele_Index
-		dc.w loc_1675E-Tele_Index
-		dc.w loc_16798-Tele_Index
+			dc.w Tele_Idle-Tele_Index
+			dc.w Tele_Charge-Tele_Index
+			dc.w Tele_Move-Tele_Index
 ; ===========================================================================
 
 Tele_Main:	; Routine 0
@@ -29,12 +34,12 @@ Tele_Main:	; Routine 0
 		andi.w	#$1E,d0
 		lea	Tele_Data(pc),a2
 		adda.w	(a2,d0.w),a2
-		move.w	(a2)+,$3A(a0)
-		move.l	a2,$3C(a0)
-		move.w	(a2)+,$36(a0)
-		move.w	(a2)+,$38(a0)
+		move.w	(a2)+,tele_coords_left(a0)
+		move.l	a2,tele_list_pos(a0)
+		move.w	(a2)+,tele_x_target(a0)
+		move.w	(a2)+,tele_y_target(a0)
 
-loc_166C8:	; Routine 2
+Tele_Idle:	; Routine 2
 		lea	(v_player).w,a1
 		move.w	obX(a1),d0
 		sub.w	obX(a0),d0
@@ -76,34 +81,35 @@ locret_1675C:
 		rts	
 ; ===========================================================================
 
-loc_1675E:	; Routine 4
-		lea	(v_player).w,a1
-		move.b	$32(a0),d0
-		addq.b	#2,$32(a0)
-		jsr	(CalcSine).l
-		asr.w	#5,d0
-		move.w	obY(a0),d2
-		sub.w	d0,d2
-		move.w	d2,obY(a1)
-		cmpi.b	#$80,$32(a0)
-		bne.s	locret_16796
+Tele_Charge:	; Routine 4
+		lea		(v_player).w,a1
+		ori.w	#$8000,obGfx(a1)
+;		move.b	$32(a0),d0
+;		addq.b	#2,$32(a0)
+;		jsr		(CalcSine).l
+;		asr.w	#5,d0
+;		move.w	obY(a0),d2
+;		sub.w	d0,d2
+;		move.w	d2,obY(a1)
+;		cmpi.b	#$80,$32(a0)
+;		bne.s	locret_16796
 		bsr.w	sub_1681C
 		addq.b	#2,obRoutine(a0)
-		sfx	sfx_Teleport	; play teleport sound
+;		sfx	sfx_Teleport	; play teleport sound
 
 locret_16796:
 		rts	
 ; ===========================================================================
 
-loc_16798:	; Routine 6
-		addq.l	#4,sp
+Tele_Move:	; Routine 6
+		addq.l	#4,sp			; skip obrange check
 		lea	(v_player).w,a1
 		subq.b	#1,$2E(a0)
-		bpl.s	loc_167DA
-		move.w	$36(a0),obX(a1)
-		move.w	$38(a0),obY(a1)
+		bpl.s	Tele_SpeedToPos
+		move.w	tele_x_target(a0),obX(a1)
+		move.w	tele_y_target(a0),obY(a1)
 		moveq	#0,d1
-		move.b	$3A(a0),d1
+		move.b	tele_coords_left(a0),d1
 		addq.b	#4,d1
 		cmp.b	$3B(a0),d1
 		bcs.s	loc_167C2
@@ -112,14 +118,14 @@ loc_16798:	; Routine 6
 ; ===========================================================================
 
 loc_167C2:
-		move.b	d1,$3A(a0)
-		movea.l	$3C(a0),a2
-		move.w	(a2,d1.w),$36(a0)
-		move.w	2(a2,d1.w),$38(a0)
+		move.b	d1,tele_coords_left(a0)
+		movea.l	tele_list_pos(a0),a2
+		move.w	(a2,d1.w),tele_x_target(a0)
+		move.w	2(a2,d1.w),tele_y_target(a0)
 		bra.w	sub_1681C
 ; ===========================================================================
 
-loc_167DA:
+Tele_SpeedToPos:
 		move.l	obX(a1),d2
 		move.l	obY(a1),d3
 		move.w	obVelX(a1),d0
@@ -140,7 +146,8 @@ loc_16800:
 		clr.b	obRoutine(a0)
 		clr.b	(f_lockmulti).w
 		move.w	#0,obVelX(a1)
-		move.w	#$200,obVelY(a1)
+		move.w	#-$800,obVelY(a1)
+		sfx     sfx_Dash                ; Play launch sound
 		rts	
 
 ; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
@@ -148,8 +155,8 @@ loc_16800:
 
 sub_1681C:
 		moveq	#0,d0
-		move.w	#$1000,d2
-		move.w	$36(a0),d0
+		move.w	#$800,d2
+		move.w	tele_x_target(a0),d0
 		sub.w	obX(a1),d0
 		bge.s	loc_16830
 		neg.w	d0
@@ -157,8 +164,8 @@ sub_1681C:
 
 loc_16830:
 		moveq	#0,d1
-		move.w	#$1000,d3
-		move.w	$38(a0),d1
+		move.w	#$800,d3
+		move.w	tele_y_target(a0),d1
 		sub.w	obY(a1),d1
 		bge.s	loc_16844
 		neg.w	d1
@@ -168,12 +175,12 @@ loc_16844:
 		cmp.w	d0,d1
 		bcs.s	loc_1687A
 		moveq	#0,d1
-		move.w	$38(a0),d1
+		move.w	tele_y_target(a0),d1
 		sub.w	obY(a1),d1
 		swap	d1
 		divs.w	d3,d1
 		moveq	#0,d0
-		move.w	$36(a0),d0
+		move.w	tele_x_target(a0),d0
 		sub.w	obX(a1),d0
 		beq.s	loc_16866
 		swap	d0
@@ -193,12 +200,12 @@ loc_16874:
 
 loc_1687A:
 		moveq	#0,d0
-		move.w	$36(a0),d0
+		move.w	tele_x_target(a0),d0
 		sub.w	obX(a1),d0
 		swap	d0
 		divs.w	d2,d0
 		moveq	#0,d1
-		move.w	$38(a0),d1
+		move.w	tele_y_target(a0),d1
 		sub.w	obY(a1),d1
 		beq.s	loc_16898
 		swap	d1
@@ -220,14 +227,29 @@ loc_168A6:
 Tele_Data:	dc.w @type00-Tele_Data, @type01-Tele_Data, @type02-Tele_Data
 		dc.w @type03-Tele_Data, @type04-Tele_Data, @type05-Tele_Data
 		dc.w @type06-Tele_Data, @type07-Tele_Data
-@type00:	dc.w 4,	$794, $98C
-@type01:	dc.w 4,	$94, $38C
-@type02:	dc.w $1C, $794,	$2E8
-		dc.w $7A4, $2C0, $7D0
-		dc.w $2AC, $858, $2AC
-		dc.w $884, $298, $894
-		dc.w $270, $894, $190
-@type03:	dc.w 4,	$894, $690
+
+@type00:	dc.w 4
+		dc.w $794, $98C
+
+@type01:	dc.w 20
+		dc.w  $70, $580
+		dc.w  $80, $590
+		dc.w $330, $590
+		dc.w $340, $5A0
+		dc.w $340, $310
+
+@type02:	dc.w $1C
+		dc.w $794, $2E8
+		dc.w $7A4, $2C0
+		dc.w $7D0, $2AC
+		dc.w $858, $2AC
+		dc.w $884, $298
+		dc.w $894, $270
+		dc.w $894, $190
+
+@type03:	dc.w 4
+		dc.w $894, $690
+
 @type04:	dc.w $1C, $1194, $470
 		dc.w $1184, $498, $1158
 		dc.w $4AC, $FD0, $4AC
