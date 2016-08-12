c65418eeec45a5ac1d65539c48638198f5399198
crashmahoney <crashmahoney@gmail.com>
2016-08-12 20:17:56 +1000

    reposition hud elements

--- _incObj/21 HUD.asm
+++ _incObj/21 HUD.asm
@@ -14,7 +14,7 @@ HUD_Index:	dc.w HUD_Main-HUD_Index
 
 HUD_Main:	; Routine 0
 		addq.b	#2,obRoutine(a0)
-		move.w	#$90,obX(a0)
+		move.w	#$110,obX(a0)
 		move.w	#$108,obScreenY(a0)
 		move.l	#Map_HUD,obMap(a0)
 		move.w	#$6CE,obGfx(a0)
--- _maps/HUD.asm
+++ _maps/HUD.asm
@@ -5,21 +5,21 @@ Map_HUD:	dc.w @ringGFX-Map_HUD, @ringGFXred-Map_HUD
 
 
 @ringGFX:	dc.b 6
-		SpriteMap  8, -120, 3, 2, 0, 0, 1, 0, 0x0022     ; ring amount
-		SpriteMap  0, -128, 2, 3, 0, 0, 1, 1, 0x0000     ; ring
-		SpriteMap  -8, -104, 2, 2, 0, 0, 1, 0, 0x0006    ; A box
-		SpriteMap  9, -104, 2, 2, 0, 0, 1, 0, 0x000A     ; B box
-;		SpriteMap  26, -104, 4, 2, 0, 0, 1, 0, 0x000E    ; C box
-		SpriteMap  26, -104, 2, 2, 0, 0, 1, 0, 0x000E    ; C box
-		SpriteMap  56, -120, 4, 4, 0, 0, 1, 0, 0x0012    ; map
-
+		SpriteMap  -112, -120, 3, 2, 0, 0, 1, 0, 0x0022     ; ring amount
+		SpriteMap  -120, -128, 2, 3, 0, 0, 1, 1, 0x0000     ; ring
+		SpriteMap  -128, -104, 2, 2, 0, 0, 1, 0, 0x0006    ; A box
+		SpriteMap  -111, -104, 2, 2, 0, 0, 1, 0, 0x000A     ; B box
+;		SpriteMap  -94, -104, 4, 2, 0, 0, 1, 0, 0x000E    ; C box
+		SpriteMap  -94, -104, 2, 2, 0, 0, 1, 0, 0x000E    ; C box
+		SpriteMap  127, -128, 4, 4, 0, 0, 1, 0, 0x0012    ; map
 		even
+
 @ringGFXred:	dc.b 6
-		SpriteMap  8, -120, 3, 2, 0, 0, 1, 0, 0x0022     ; ring amount
-		SpriteMap  0, -128, 2, 3, 0, 0, 1, 0, 0x0000     ; ring
-		SpriteMap  -8, -104, 2, 2, 0, 0, 1, 0, 0x0006    ; A box
-		SpriteMap  9, -104, 2, 2, 0, 0, 1, 0, 0x000A     ; B box
-;		SpriteMap  26, -104, 4, 2, 0, 0, 1, 0, 0x000E    ; C box
-		SpriteMap  26, -104, 2, 2, 0, 0, 1, 0, 0x000E    ; C box
-		SpriteMap  56, -120, 4, 4, 0, 0, 1, 0, 0x0012    ; map
+		SpriteMap  -112, -120, 3, 2, 0, 0, 1, 0, 0x0022     ; ring amount
+		SpriteMap  -120, -128, 2, 3, 0, 0, 1, 0, 0x0000     ; ring
+		SpriteMap  -128, -104, 2, 2, 0, 0, 1, 0, 0x0006    ; A box
+		SpriteMap  -111, -104, 2, 2, 0, 0, 1, 0, 0x000A     ; B box
+;		SpriteMap  -94, -104, 4, 2, 0, 0, 1, 0, 0x000E    ; C box
+		SpriteMap  -94, -104, 2, 2, 0, 0, 1, 0, 0x000E    ; C box
+		SpriteMap  127, -128, 4, 4, 0, 0, 1, 0, 0x0012    ; map
 		even
