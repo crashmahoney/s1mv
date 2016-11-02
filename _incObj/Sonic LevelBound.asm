11ad9e56f6138631657d548007248b1f047a26ef
crashmahoney <crashmahoney@gmail.com>
2016-11-02 16:01:43 +1100

    optimized/tidied up level transition code

--- _incObj/Sonic LevelBound.asm
+++ _incObj/Sonic LevelBound.asm
@@ -52,10 +52,9 @@ Sonic_LevelBound:			; XREF: Obj01_MdNormal; et al
 		blt.s	@bottom		; if yes, branch
 
 	@chktop:
-;		move.w	(v_limittop2).w,d0
-;		addi.w	#$E0,d0
-		cmp.w	#0,obY(a0)	; has Sonic touched the	top boundary?
-		blt.w	@top		; if yes, branch
+		move.w	(v_limittop2).w,d0
+		cmp.w	obY(a0),d0	; has Sonic touched the	top boundary?
+		bgt.w	@top		; if yes, branch
 @rts:
 		rts
 ; ===========================================================================
@@ -65,40 +64,28 @@ Sonic_LevelBound:			; XREF: Obj01_MdNormal; et al
 		move.w	($FFFFF72E).w,d1	; +++
 		cmp.w	d0,d1				; +++ screen still scrolling down?
 		bge.w	@bottom_cont		; +++ if not, continue
+		rts
 @bottom_cont:
-		lea     BottomData,a3
+		lea     (BottomData).l,a3
 		bra.s   @load1
 @top:
-		lea     TopData,a3
-	  
-	  @load1:
-		clr.b	(v_lastlamp).w	                 ; clear lamppost counter
-		move.b  #1,(f_dontstopmusic).w           ; keep playing current song
-		move.b	#$FF,(v_lastlamp).w 	         ; lamppost number
-		move.b	(v_lastlamp).w,($FFFFFE31).w
-		jsr     SaveState                        ; Save act's state to SRAM
-		moveq	#0,d3
-		move.b	(v_zone).w,d3                    ; get zone number
-		mulu    #4,d3                            ; mult by 4 to get index number of first act in zone
-		add.b   (v_act).w,d3                     ; add act number to get final index number
-		add.w   d3,d3                            ; double (cos they're stored as words)
-		move.w	(a3,d3.w),d3					; data offset from table
-		lea     (a3,d3.w),a3                     ; sets a3 to address of data
-
+		lea     (TopData).l,a3
+	@load1:
+		move.w	(v_zone).w,d3
+		lsl.b	#6,d3
+		lsr.w	#5,d3
+		adda.w	(a3,d3.w),a3			; load the pointer to the current act
 	@chk_x:
-		move.w	(a3)+,d1					; get max x pos for this transition
-		sub.w	obX(a0),d1					; subtract sonic's y pos
-		bcc.s	@x_ok						; if still greater than 0, branch
-		adda.w	#12,a3						; advance to next transition
-		bra.w	@chk_x						; loop
-
+		move.w	(a3)+,d1			; get max x pos for this transition
+		sub.w	obX(a0),d1			; subtract sonic's y pos
+		bcc.s	@x_ok				; if still greater than 0, branch
+		adda.w	#12,a3				; advance to next transition
+		bra.w	@chk_x				; loop
 	@x_ok:
-		move.b  (a3),(f_dontstopmusic).w        ; load graphics/change music?
-		adda    #2,a3                           ; advance data location
+		move.w  (a3)+,(f_dontstopmusic).w       ; load graphics/change music?
 		move.w	(a3)+,($FFFFFE34).w		; y-position
 		move.w	obX(a0),d1
 		add.w	(a3)+,d1         		; put x offset in d1
-;		andi.w	#$FFF,d1
 		move.w	d1,($FFFFFE32).w                ; add offset to y position
 		move.w	(a3)+,(v_lamp_limitbtm).w       ; screen bottom limit
 		move.b	(a3),($FFFFFE3C).w              ; routine counter for dynamic level mod
@@ -111,44 +98,21 @@ Sonic_LevelBound:			; XREF: Obj01_MdNormal; et al
 @rightside:
 		lea     RightData,a3
 		bra.s   @load2
-
 @leftside:
-
-		cmpi.b  #$02,(v_act).w                   ; act 3?
-		bne.s   @loadleftdata                    ;
-		move.w  #$1000,d0                        ; ***(this bit is a hack to stop level from changing at the wrong point in act 3)
-		move.w  obX(a0),d1                       ;
-		cmp.w   d0,d1                            ; is sonic far enough into the level that going off the left doesn't matter? (d1 is sonic's pos)
-		ble.s   @loadleftdata
-		rts
-
-	  @loadleftdata:
 		lea     LeftData,a3
-	  
-	  @load2:
-		clr.b	(v_lastlamp).w	                 ; clear lamppost counter
-		move.b  #1,(f_dontstopmusic).w           ; keep playing current song
-		move.b	#$FF,(v_lastlamp).w 	         ; lamppost number
-		move.b	(v_lastlamp).w,($FFFFFE31).w
-		jsr     SaveState                        ; Save act's state to SRAM
-		moveq	#0,d3
-		move.b	(v_zone).w,d3                    ; get zone number
-		mulu    #4,d3                            ; mult by 4 to get index number of first act in zone
-		add.b   (v_act).w,d3                     ; add act number to get final index number
-		add.w   d3,d3                            ; double (cos they're stored as words)
-		move.w	(a3,d3.w),d3					; data offset from table
-		lea     (a3,d3.w),a3                     ; sets a3 to address of data
-
+	@load2:
+		move.w	(v_zone).w,d3
+		lsl.b	#6,d3
+		lsr.w	#5,d3
+		adda.w	(a3,d3.w),a3			; load the pointer to the current act
 	@chk_y:
-		move.w	(a3)+,d1					; get max y pos for this transition
-		sub.w	obY(a0),d1					; subtract sonic's y pos
-		bcc.s	@y_ok						; if still greater than 0, branch
-		adda.w	#12,a3						; advance to next transition
-		bra.w	@chk_y						; loop
-
+		move.w	(a3)+,d1			; get max y pos for this transition
+		sub.w	obY(a0),d1			; subtract sonic's y pos
+		bcc.s	@y_ok				; if still greater than 0, branch
+		adda.w	#12,a3				; advance to next transition
+		bra.w	@chk_y				; loop
 	@y_ok:
-		move.b  (a3),(f_dontstopmusic).w        ; load graphics/change music?
-		adda    #2,a3                           ; advance data location
+		move.w  (a3)+,(f_dontstopmusic).w	; load graphics/change music?
 		move.w	(a3)+,($FFFFFE32).w		; x-position
 		move.w	obY(a0),d1
 		add.w	(a3)+,d1         		; put y offset in d1
@@ -159,8 +123,9 @@ Sonic_LevelBound:			; XREF: Obj01_MdNormal; et al
 		adda    #2,a3                           ; advance data location
 		move.w	(a3),d0                         ; set level
 ; ===========================================================================
-
 @load_finish:
+		move.b	#$FF,(v_lastlamp).w 	         ; lamppost number
+		move.b	(v_lastlamp).w,($FFFFFE31).w
 		move.w	(v_rings).w,($FFFFFE36).w 	; rings
 		move.b	(v_lifecount).w,($FFFFFE54).w 	; lives
 		move.l	(v_time).w,($FFFFFE38).w 	; time
@@ -175,16 +140,16 @@ Sonic_LevelBound:			; XREF: Obj01_MdNormal; et al
 		move.b	(v_player+obStatus2).w,(v_lamp_status2).w
 		move.b  #0,(v_lamp_dir).w               ; clear dir flag
 		btst    #0,(v_player+obStatus).w        ; test which way sonic is facing
-		beq.s   @chkroll4                       ; if left, branch
+		beq.s   @chkroll                       ; if left, branch
 		move.b  #1,(v_lamp_dir).w               ; set if facing right
 
-	 @chkroll4:
+	 @chkroll:
 		move.b  #0,(v_lamp_roll).w               ; clear roll flag
 		btst    #2,(v_player+obStatus).w         ; test if sonic is rolling
-		beq.s   @notrolling4                     ; if not, branch
+		beq.s   @notrolling                     ; if not, branch
 		move.b  #1,(v_lamp_roll).w               ; set roll flag
 
-	 @notrolling4:
+	 @notrolling:
 		move.w	d0,(v_zone).w                    ; set level     +++
 		move.w	#1,(f_restart).w                 ; restart the level
 		rts
